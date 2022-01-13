1b7a1a32673752274a2bd901a7b8bb1855c94238
omsi96 <omsi96@gmail.com>
2022-01-13 17:25:15 +0300

    localization

---
+++ localization.js
@@ -0,0 +1,7 @@
+const ar = require("./locals.ar.json");
+const en = require("./locals.en.json");
+const es = require("./locals.es.json");
+const languages = { ar, en, es };
+
+console.log(languages.ar.myProfile);
+console.log(languages.ar.orderNow);
